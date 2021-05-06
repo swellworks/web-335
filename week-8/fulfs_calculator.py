c87301b0de6755117bd0bc8758e124df25fb9f56
Perry Fulfs <pfulfs@my365.bellevue.edu>
2021-05-05 21:33:53 -0500

    week-8 commit the changes to all files
    commit the changes to all files

---
+++ week-8/fulfs_calculator.py
@@ -0,0 +1,21 @@
+
+#============================================
+# Title: Exercise 8.3
+# Author: Richard Krasso
+# Date: 05 May 2021
+# Modified By: Perry Fulfs
+# Description: Python in Action
+#===========================================
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def divide(num1, num2):
+    return num1/num2
+
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
