38a43e7511afa787bf28b11aad01a2ee0a70e527
Perry Fulfs <pfulfs@my365.bellevue.edu>
2021-05-10 20:36:53 -0500

    commit user serv

---
+++ week-9/fulfs-user-service.py
@@ -0,0 +1,23 @@
+from pymongo import MongoClient
+
+import pprint
+
+import datetime
+
+client=MongoClient('localhost', 27017)
+
+db=client.web335
+
+user={
+    "first_name": "Doug",
+    "last_name": "Giblets",
+    "email": "dgiblets@bigfoothunter.com",
+    "employee_id": "2298405",
+    "date_created": datetime.datetime.utcnow()
+}
+
+user_id = db.users.insert_one(user).inserted_id
+
+print(user_id)
+
+pprint.pprint(db.users.find_one({"employee_id": "2298405"}))
